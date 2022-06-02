1ac73e367318058339d108e65a88ec96eeff3139
niteshkr034 <81917957+niteshkr034@users.noreply.github.com>
2022-06-02 10:56:06 +0530

    7th Challenge

--- script.js
+++ script.js
@@ -6,27 +6,14 @@
 
 ////////////////////////////
 
-let arr = [-4, 3, -9, 0, 4, 1]
-
-function plusMinus(arr) {
-  let p = 0;
-  let n = 0;
-  let z = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      p++;
-    } else if (arr[i] < 0) {
-      n++;
-    } else if (arr[i] == 0) {
-      z++;
-    }
+function staircase(n) {
+  // Write your code here
+  let space=""
+  let hash=""
+  for (let i = 1; i <= n ; i++) {
+    space = " ".repeat(n - i);
+    hash = "#".repeat(i);
+    console.log(space+hash);
   }
-  p= (p/arr.length).toFixed(6)
-  n= (n/arr.length).toFixed(6)
-  z= (z/arr.length).toFixed(6)
-  console.log(p)
-  console.log(n)
-  console.log(z)
 }
-plusMinus(arr);
+staircase(5);
